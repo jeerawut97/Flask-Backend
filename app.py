906b86004f6aaa70ae69b4b2877a814cd1052ef6
jeerawut97 <jeerawut.jd40@gmail.com>
2022-03-11 19:53:41 +0700

    change db connect

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-import json
+import json, os
 
 app = Flask(__name__)
 app.secret_key = 'jose'
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
