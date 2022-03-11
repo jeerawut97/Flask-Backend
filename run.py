6cfbf47146ac0f57ec162d4a277ee15e182529a2
jeerawut97 <jeerawut.jd40@gmail.com>
2022-03-11 19:42:37 +0700

    fix deploy

--- run.py
+++ run.py
@@ -1,40 +1,8 @@
-from flask import Flask, jsonify
-from flask_restful import Api
-from flask_jwt import JWT
-from datetime import timedelta
+from app import app
+from db import db
 
-from security import authenticate, identity
-from resources.user import UserRegister
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
-import json
-
-app = Flask(__name__)
-app.secret_key = 'jose'
-api = Api(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_AUTH_URL_RULE'] = '/login'
-app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
-# app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+db.init_app(app)
 
 @app.before_first_request
 def create_table():
     db.create_all()
-
-jwt = JWT(app, authenticate, identity)
-@jwt.auth_response_handler
-def customized_response_handler(access_token, identity):
-    return jsonify({'access_token': access_token.decode('utf-8'),'user_id': identity.id})
-
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
-api.add_resource(UserRegister, '/register')
-
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-    app.run(port=5000, debug=True)
