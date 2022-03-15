bd11754629d5f707555fd839e93f3212e213e8f9
jeerawut97 <jeerawut.jd40@gmail.com>
2022-03-15 18:25:40 +0700

    remove flask_jwt

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,5 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
-from flask_jwt import jwt_required
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import create_access_token, create_refresh_token
 
