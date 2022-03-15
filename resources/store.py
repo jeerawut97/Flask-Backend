91f2b6f8515436b77f747aaaab73c9424b6c2afe
jeerawut97 <jeerawut.jd40@gmail.com>
2022-03-15 18:27:11 +0700

    remove flask_jwt

--- resources/store.py
+++ resources/store.py
@@ -1,16 +1,16 @@
 from flask_restful import Resource
 from models.store import StoreModel
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 
 class Store(Resource):
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         store = StoreModel.find_by_name(name)
         if store:
             return store.json()
         return {'message': 'Store not found'}, 404
 
-    @jwt_required()
+    @jwt_required
     def post(self, name):
         if StoreModel.find_by_name(name):
             return {'message': "A store with name '{}' already exists.".format(name)}, 400
@@ -23,7 +23,7 @@ class Store(Resource):
 
         return store.json(), 201
 
-    @jwt_required()
+    @jwt_required
     def delete(self, name):
         store = StoreModel.find_by_name(name)
         if store:
@@ -33,6 +33,6 @@ class Store(Resource):
 
 
 class StoreList(Resource):
-    @jwt_required()
+    @jwt_required
     def get(self):
         return {'stores': [x.json() for x in StoreModel.find_all()]}
