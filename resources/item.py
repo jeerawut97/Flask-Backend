8f645717ca4d47d3798d93fb23b631818cc2518f
jeerawut97 <jeerawut.jd40@gmail.com>
2022-03-15 19:13:57 +0700

    change value jwt_requried()

--- resources/item.py
+++ resources/item.py
@@ -15,14 +15,14 @@ class Item(Resource):
                         help="Every item needs a store_id."
                         )
 
-    @jwt_required
+    @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json()
         return {'message': 'Item not found'}, 404
 
-    @jwt_required
+    @jwt_required()
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
@@ -38,7 +38,7 @@ class Item(Resource):
 
         return item.json(), 201
 
-    @jwt_required
+    @jwt_required()
     def delete(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -46,7 +46,7 @@ class Item(Resource):
             return {'message': 'Item deleted.'}
         return {'message': 'Item not found.'}, 404
 
-    @jwt_required
+    @jwt_required()
     def put(self, name):
         data = Item.parser.parse_args()
 
@@ -63,6 +63,6 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    @jwt_required
+    @jwt_required()
     def get(self):
         return {'items': [x.json() for x in ItemModel.find_all()]}
