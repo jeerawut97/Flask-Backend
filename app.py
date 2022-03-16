b3e5b89f33a6512ea0cc54177d57c1f96624fb32
jeerawut97 <jeerawut.jd40@gmail.com>
2022-03-16 12:24:03 +0700

    fix header payload

--- app.py
+++ app.py
@@ -45,32 +45,30 @@ def add_claims_to_jwt(identity):
 
 @jwt.token_in_blocklist_loader
 def check_if_token_in_blacklist(jwt_header, jwt_payload):
-    print(jwt_payload)
     jti = jwt_payload["jti"]
-    print(jti)
     # token_in_redis = jwt_redis_blocklist.get(jti)
     return jti in BLACKLIST
     # return token_in_redis is not None
     # return decrpted_token['identity'] in BLACKLIST
 
 @jwt.expired_token_loader
-def expired_token_callback():
+def expired_token_callback(jwt_header, jwt_payload):
     return jsonify({'description':'The token has expired.', 'error':'token_expired'}), 401
 
 @jwt.invalid_token_loader
-def invalid_token_callback(error):
+def invalid_token_callback(jwt_header, jwt_payload):
     return jsonify({'description':'Signature verification failed.', 'error':'invalid_token'}), 401
 
 @jwt.unauthorized_loader
-def missing_token_callback(error):
+def missing_token_callback(jwt_header, jwt_payload):
     return jsonify({'description':'Request does not contain an access token.', 'error':'authorization_required'}), 401
 
 @jwt.needs_fresh_token_loader
-def token_notfresh_callback(error):
+def token_notfresh_callback(jwt_header, jwt_payload):
     return jsonify({'description':'The token is not fresh.', 'error':'fresh_token_required'}), 401
 
 @jwt.revoked_token_loader
-def revoked_token_callback(error):
+def revoked_token_callback(jwt_header, jwt_payload):
     return jsonify({'description':'The Token has  been revoked.', 'error':'token_revoked'}), 401
 
 api.add_resource(Item, '/item/<string:name>')
